e9bf619c34c216fdc64e5db219d6ff1a839f7fd7
Luca Bercioux <Lucabex@gmail.com>
2026-09-04 10:52:41 +0100

    commit after saving

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using auth6.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder =WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
@@ -7,6 +10,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var JwtSetings = builder.Configuration.GetSection("JwtSetings");
+var secretKey = JwtSetings["secretKey"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey =true,
+        ValidIssuer = JwtSetings["Issuer"],
+        ValidAudience = JwtSetings["Audience"],
+        IssuerSigningKey= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+    };
+});
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -14,7 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
 
